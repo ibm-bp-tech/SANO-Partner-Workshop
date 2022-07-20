8a694618bc840bbc2e433a6c2c90b9d848616ebe
Dasakharuk <Daniel.Sakharuk@ibm.com>
2022-07-20 11:40:17 -0400

    minor changes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,11 +5,11 @@ import { calloutLink } from './Homepage.module.scss';
 
 import Carbon from './logo.png';
 
-const FirstLeftText = () => <p>IBM Observability by Instana</p>;
+const FirstLeftText = () => <p>IBM SevOne Workshop</p>;
 
 const FirstRightText = () => (
   <p>
-    Learn about IBM's Instana. This site contains example labs, tutorials, and partner instructions.
+    Learn about IBM's SevOne. This site contains example labs, tutorials, and partner instructions.
   </p>
 );
 
